10ff8258874605fc2ba944a2875f6ee8e62a964c
Mehmet Ali Er <mehmetalier83@hotmail.com>
2024-01-24 16:09:35 +0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Mehmet Ali Er on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
